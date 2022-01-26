da34084390b5dbb1b7f61de40a781baafe266f4d
aleksi bielski <abielski@gaming.tech>
2022-01-26 16:03:04 +0100

    exercice 3 debut

--- exercice1.cpp
+++ exercice1.cpp
@@ -1,8 +1,6 @@
 #include "exercice1.hpp"
 #include <SDL2/SDL.h>
 
-#include <string>
-
 #include <iostream>
 using namespace std;
 
@@ -18,23 +16,31 @@ MainSDLWindow::~MainSDLWindow(void){
 
 }
 
-int MainSDLWindow::Init(string title_window, int largeur,int hauteur){
-    window = SDL_CreateWindow( "title_window" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , largeur,hauteur, SDL_WindowFlags());
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer,0,0,0, SDL_ALPHA_OPAQUE);
-
-    if (window == NULL)
-    {
-
-        cout << "Error: SDL-CreateWindow() failed \n";
-        return EXIT_FAILURE;
-    }
+int MainSDLWindow::Init(const char *name, int largeur,int hauteur){
 
     if (SDL_Init(SDL_INIT_VIDEO)<0)
     {
-        cout << "Error: INIT() function failed \n";
+        cout << "Error: INIT() function failed";
         return EXIT_FAILURE;
     }
+
+    this->window = SDL_CreateWindow( name , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , largeur,hauteur, SDL_WindowFlags());
+    if (window == NULL) {
+        cout << "Error: SDL-CreateWindow() failed";
+        return EXIT_FAILURE;
+    }
+
+    this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        cout << "Error: SDL-CreateRenderer() failed";
+        return EXIT_FAILURE;
+    }
+
+
+    SDL_SetRenderDrawColor(renderer,0,0,0, SDL_ALPHA_OPAQUE);
+
+
+
     return EXIT_SUCCESS;
 }
 
@@ -46,3 +52,44 @@ int main(void) {
     MainSDLWindow main_window;
     main_window.Init("salut", 200,200);
 }
+
+
+
+    Uint32 frame_rate = 20;
+    Uint32 frame_time_start = SDL_GetTicks();
+    void keyboard() {
+
+
+
+        const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+
+        int x=0;
+        int y=0;
+        int move_x =0;
+        int move_y =0;
+        SDL_Rect();
+        SDL_Rect r;
+        r.x = 50;
+        r.y = 50;
+        r.w = 50;
+        r.h = 50;
+
+        if (keystates[SDL_SCANCODE_UP]) {
+            move_x = 0;
+            move_y =1;
+        }
+        if (keystates[SDL_SCANCODE_DOWN]) {
+            move_x = 0;
+            move_y =-1;
+        }
+        if (keystates[SDL_SCANCODE_LEFT]) {
+            move_x = -1;
+            move_y = 0;
+        }
+        if (keystates[SDL_SCANCODE_RIGHT]) {
+            move_x = 1;
+            move_y = 0;
+        }
+        SDL_Delay(SDL_GetTicks() - frame_time_start);
+    }
+    
--- exercice1.hpp
+++ exercice1.hpp
@@ -1,12 +1,11 @@
 #include <SDL2/SDL.h>
-#include <string>
 using namespace std;
 
 class MainSDLWindow {
 public:
     MainSDLWindow();
     ~MainSDLWindow();
-    int Init( string name, int largeur, int hauteur);
+    int Init(const char *name, int largeur, int hauteur);
     SDL_Renderer * GetRenderer(void);
 private:
     SDL_Window * window;
