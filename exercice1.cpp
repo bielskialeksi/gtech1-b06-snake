2724454dbbc154dd3e744ea2d48448fdbbbd540f
aleksi bielski <abielski@gaming.tech>
2022-01-28 12:46:13 +0100

    travail exercice3

--- exercice1.cpp
+++ exercice1.cpp
@@ -7,6 +7,11 @@ using namespace std;
 #include <stdio.h>
 #include <stdlib.h>
 
+int move_x = 1;
+int move_y = 0;
+
+
+
 MainSDLWindow::MainSDLWindow(void){
     window = NULL;
     renderer = NULL;
@@ -48,48 +53,65 @@ SDL_Renderer * MainSDLWindow::GetRenderer(void){
     return this->renderer;
 }
 
-int main(void) {
-    MainSDLWindow main_window;
-    main_window.Init("salut", 200,200);
+void keyboard() {
+
 }
 
 
 
-    Uint32 frame_rate = 20;
-    Uint32 frame_time_start = SDL_GetTicks();
-    void keyboard() {
 
+int main(void) {
+    MainSDLWindow main_window;
+    main_window.Init("salut", 500,500);
 
+    SDL_Rect srcrect;
+        
+    srcrect.x = 250;
+    srcrect.y = 250;
+    srcrect.w = 32;
+    srcrect.h = 32;
+
+    while( srcrect.x<468 && srcrect.x>0 && srcrect.y<500-32 && srcrect.y>0 ){
+
+        Uint32 frame_rate = 20;
+        Uint32 frame_time_start = SDL_GetTicks();
+        srcrect.x = srcrect.x+move_x;
+        srcrect.y = srcrect.y+move_y;
+        SDL_SetRenderDrawColor( main_window.GetRenderer(), 25, 25, 25, 0 );
+
+        // Clear winow
+        SDL_RenderClear(  main_window.GetRenderer() );
+        SDL_SetRenderDrawColor( main_window.GetRenderer(), 255, 0, 0, 255 );
+
+        SDL_RenderFillRect(  main_window.GetRenderer(), &srcrect );
+
+        SDL_RenderPresent( main_window.GetRenderer());
 
         const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-
-        int x=0;
-        int y=0;
-        int move_x =0;
-        int move_y =0;
-        SDL_Rect();
-        SDL_Rect r;
-        r.x = 50;
-        r.y = 50;
-        r.w = 50;
-        r.h = 50;
-
         if (keystates[SDL_SCANCODE_UP]) {
             move_x = 0;
             move_y =1;
+            cout << "up ok ";
         }
         if (keystates[SDL_SCANCODE_DOWN]) {
             move_x = 0;
             move_y =-1;
-        }
+            cout << "down ok ";
+            }
         if (keystates[SDL_SCANCODE_LEFT]) {
             move_x = -1;
             move_y = 0;
+            cout << "left ok ";
         }
         if (keystates[SDL_SCANCODE_RIGHT]) {
             move_x = 1;
             move_y = 0;
+            cout << "right ok ";
         }
+        
         SDL_Delay(SDL_GetTicks() - frame_time_start);
-    }
-    
+        }
+    cout << srcrect.x<<" "<<srcrect.y;
+    cout << main_window.GetRenderer();
+    main_window.~MainSDLWindow();
+}
