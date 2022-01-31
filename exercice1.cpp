c802ec9e58fcb245bda588679b91c21b1d78b3b8
aleksi bielski <abielski@gaming.tech>
2022-01-31 09:10:22 +0100

    -->liste chainée.

--- exercice1.cpp
+++ exercice1.cpp
@@ -40,12 +40,7 @@ int MainSDLWindow::Init(const char *name, int largeur,int hauteur){
         cout << "Error: SDL-CreateRenderer() failed";
         return EXIT_FAILURE;
     }
-
-
     SDL_SetRenderDrawColor(renderer,0,0,0, SDL_ALPHA_OPAQUE);
-
-
-
     return EXIT_SUCCESS;
 }
 
@@ -58,32 +53,26 @@ void MainSDLWindow::clear()
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
 }
+
 void keyboard() {
-      const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-        if (keystates[SDL_SCANCODE_UP] &&  move_y !=10) {
-            move_x = 0;
-            move_y =-10;
-   
-            }
-        if (keystates[SDL_SCANCODE_DOWN]  &&  move_y != -10){
-            move_x = 0;
-            move_y =10;
-  
-            }
-        if (keystates[SDL_SCANCODE_LEFT]  &&  move_x != 10){
-            move_x = -10;
-            move_y = 0;
-
+    const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+    if (keystates[SDL_SCANCODE_UP] &&  move_y !=10) {
+        move_x = 0;
+        move_y =-10;
         }
-        if (keystates[SDL_SCANCODE_RIGHT]  &&  move_x != -10){
-            move_x = 10;
-            move_y = 0;
-   
-        }
+    if (keystates[SDL_SCANCODE_DOWN]  &&  move_y != -10){
+        move_x = 0;
+        move_y =10;
+    }
+    if (keystates[SDL_SCANCODE_LEFT]  &&  move_x != 10){
+        move_x = -10;
+        move_y = 0;
+    }
+    if (keystates[SDL_SCANCODE_RIGHT]  &&  move_x != -10){
+        move_x = 10;
+        move_y = 0;
+    }
 }
-
-
-
 
 int main(void) {
     MainSDLWindow main_window;
@@ -97,14 +86,11 @@ int main(void) {
     srcrect.y = 250;
     srcrect.w = 20;
     srcrect.h = 20;
-//srcrect.x<468 && srcrect.x>0 && srcrect.y<500-32 && srcrect.y>0 
     while(true){
         SDL_Delay(20);
         int frame_time_start = SDL_GetTicks();
-        /*Uint32 frame_rate = 20;
-        Uint32 frame_time_start = SDL_GetTicks();*/
-        srcrect.x = srcrect.x+move_x;
 
+        srcrect.x = srcrect.x+move_x;
         srcrect.y = srcrect.y+move_y;
 
         SDL_SetRenderDrawColor( main_window.GetRenderer(), 25, 25, 25, 0 );
@@ -119,27 +105,17 @@ int main(void) {
         keyboard();
   
    
-        //SDL_Delay(SDL_GetTicks() - frame_time_start);
-        
-
-
- 
-            SDL_Event event;
-            if (SDL_PollEvent(&event)){
-                if (event.type == SDL_QUIT)
-                {
-                    main_window.clear();
-                    break;
-                    
-                }
+        SDL_Event event;
+        if (SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT){
                 
-            }
-            
+                main_window.clear();
+                break;                
+            }      
+        }    
         if (srcrect.x<0 ||srcrect.y<0 ||srcrect.x > largeur-srcrect.w || srcrect.y >hauteur-srcrect.h){
             break;
         }
-     
-
     SDL_Delay(SDL_GetTicks() - frame_time_start );
 
     }
